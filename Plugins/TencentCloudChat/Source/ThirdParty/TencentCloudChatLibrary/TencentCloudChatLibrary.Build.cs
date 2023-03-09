4ec4ddab24895ee4d65349d0c0339da64fc23ac5
xingchenhe <xingchenhe@tencent.com>
2023-03-09 11:53:12 +0800

    rm pc source

--- Plugins/TencentCloudChat/Source/ThirdParty/TencentCloudChatLibrary/TencentCloudChatLibrary.Build.cs
+++ Plugins/TencentCloudChat/Source/ThirdParty/TencentCloudChatLibrary/TencentCloudChatLibrary.Build.cs
@@ -12,25 +12,25 @@ public class TencentCloudChatLibrary : ModuleRules
 		if (Target.Platform == UnrealTargetPlatform.Win64)
 		{
 			// Add the import library
-			PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "x64", "Release", "ExampleLibrary.lib"));
+			// PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "x64", "Release", "ExampleLibrary.lib"));
 
-			// Delay-load the DLL, so we can load it from the right place first
-			PublicDelayLoadDLLs.Add("ExampleLibrary.dll");
+			// // Delay-load the DLL, so we can load it from the right place first
+			// PublicDelayLoadDLLs.Add("ExampleLibrary.dll");
 
-			// Ensure that the DLL is staged along with the executable
-			RuntimeDependencies.Add("$(PluginDir)/Binaries/ThirdParty/TencentCloudChatLibrary/Win64/ExampleLibrary.dll");
+			// // Ensure that the DLL is staged along with the executable
+			// RuntimeDependencies.Add("$(PluginDir)/Binaries/ThirdParty/TencentCloudChatLibrary/Win64/ExampleLibrary.dll");
         }
         else if (Target.Platform == UnrealTargetPlatform.Mac)
         {
-            PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Mac", "Release", "libExampleLibrary.dylib"));
-            RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/TencentCloudChatLibrary/Mac/Release/libExampleLibrary.dylib");
+            // PublicDelayLoadDLLs.Add(Path.Combine(ModuleDirectory, "Mac", "Release", "libExampleLibrary.dylib"));
+            // RuntimeDependencies.Add("$(PluginDir)/Source/ThirdParty/TencentCloudChatLibrary/Mac/Release/libExampleLibrary.dylib");
         }
         else if (Target.Platform == UnrealTargetPlatform.Linux)
 		{
-			string ExampleSoPath = Path.Combine("$(PluginDir)", "Binaries", "ThirdParty", "TencentCloudChatLibrary", "Linux", "x86_64-unknown-linux-gnu", "libExampleLibrary.so");
-			PublicAdditionalLibraries.Add(ExampleSoPath);
-			PublicDelayLoadDLLs.Add(ExampleSoPath);
-			RuntimeDependencies.Add(ExampleSoPath);
+			// string ExampleSoPath = Path.Combine("$(PluginDir)", "Binaries", "ThirdParty", "TencentCloudChatLibrary", "Linux", "x86_64-unknown-linux-gnu", "libExampleLibrary.so");
+			// PublicAdditionalLibraries.Add(ExampleSoPath);
+			// PublicDelayLoadDLLs.Add(ExampleSoPath);
+			// RuntimeDependencies.Add(ExampleSoPath);
 		}
 		else if (Target.Platform == UnrealTargetPlatform.IOS)
         {   
